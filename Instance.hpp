d4b7bc452d5ad966f0061b7ca66ed8c669178491
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2024-09-27 08:05:51 -0300

    refactor: remove unused libraries

--- Instance.hpp
+++ Instance.hpp
@@ -1,17 +1,7 @@
 #pragma once
 
-#include <fstream>
 #include <string.h>
 #include <vector>
-#include <set>
-#include <cassert>
-#include <climits>
-#include <iostream>
-#include <ilcplex/cplex.h>
-#include <ilcplex/ilocplex.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 using namespace std;
 
