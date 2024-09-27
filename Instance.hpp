aefa1c52a869830b3ba8cd8bc3deceae44bc7c5b
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2024-09-27 08:29:51 -0300

    docs: describe functions

--- Instance.hpp
+++ Instance.hpp
@@ -9,21 +9,21 @@ class Inst {
 public:
 	void parse(const string& instPath);
 
-	unsigned nJobs;
-	unsigned nMach;
-	unsigned nOpers;
+	unsigned nJobs;								// number of jobs
+	unsigned nMach;								// number of machines
+	unsigned nOpers;							// number of operations
 
-	vector<unsigned> costs;
-	vector<unsigned> deadlines;
-	vector<double> earlCoefs;
-	vector<double> tardCoefs;
+	vector<unsigned> costs;				// costs[o] is cost of operation o
+	vector<unsigned> deadlines;		// deadlines[o] is deadline of operation o
+	vector<double> earlCoefs;			// earlCoefs[o] is earliness penalty coeficient of operation o
+	vector<double> tardCoefs;			// tardCoefs[o] is tardiness penalty coeficient of operation o
 
-	vector<unsigned> next;
-	vector<unsigned> prev;
+	vector<unsigned> next;				// next[o] is successor operation of o. 0 means no successor.
+	vector<unsigned> prev;				// prev[o] is predecessor operation of o. 0 means no predecessor.
 
-	vector<unsigned> opToMach;
-	vector<unsigned> opToJob;
+	vector<unsigned> opToMach;		// opToMach[o] is machine of o.
+	vector<unsigned> opToJob;			// opToJob[o] is job of o.
 
-	vector<unsigned> roots;
-	vector<unsigned> leafs;
+	vector<unsigned> roots;				// roots[j] is first operation of job j.
+	vector<unsigned> leafs;				// leafs[j] is last operation of job j.
 };
--- Solver.hpp
+++ Solver.hpp
@@ -7,30 +7,43 @@ using namespace std;
 class Solver {
 public:
 
+	// sets initial solution to current sequence
 	void gifflerThompson();
 
+	// current solution is feasible?
 	bool verifySchedule();
 
+	// uses cplex to schedule seq and puts this schedule in starts. Returns total penalties
 	static double scheduler(vector<unsigned> seq, vector<unsigned>& starts);
 
+	// uses cplex to schedule seq but relaxing constraints of two machines and put the obtained schedule in starts. Returns total penalties
 	static double relax_1(vector<unsigned>& seq, vector<unsigned>& starts);
 
+	// uses cplex to schedule seq but relaxing constraints of randomly selected operations and put the obtained schedule in starts. Returns total penalties
 	static double relax_2(vector<unsigned>& seq, vector<unsigned>& starts);
 
+	// transform seq in a neighbour by using insert procedure. Put the obtained schedule in starts. Return total penalties.
 	static double insert(vector<unsigned>& seq, vector<unsigned>& starts);
 	
+	// transform seq in a neighbout by using swap procedure. Put the obtained schedule in starts. Return total penalties. 
 	static double swap(vector<unsigned>& seq, vector<unsigned>& starts);
 
+	// try to improve solution represented by seq, starts and targetPenalties using neighbourhood Nk
 	void local_search(vector<unsigned>& seq, vector<unsigned>& starts, double& targetPenalties, unsigned k);
 
+	// try to improve current solution
 	void vns();
 	
+	// solve JIT-JSS problem in file instPath
 	void solve(string instPath);
 
+	// if oper is the first element of some relaxedBlocks[i] returns i. If not return -1
 	static int isFirstInSomeBlock(const vector<vector<unsigned>>& relaxedBlocks, const unsigned oper);
 
+	// returns relaxation center
 	static unsigned rc();
 
+	// returns relaxation radius
 	static unsigned rr();
 
 	vector<unsigned> sequence;
